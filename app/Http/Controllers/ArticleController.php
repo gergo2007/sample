ae32b3d1b4f28a159c4ef95ce4b721720bc393de
Katyi Gergo <gkatyi@voice.ai>
2023-09-04 17:00:29 +0300

    limit

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Resources\ArticleResource;
 use App\Models\Article;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ArticleController extends Controller
 {
@@ -26,10 +27,41 @@ class ArticleController extends Controller
     /**
      * Display the specified resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function show($id)
     {
+        if (!\Auth::check()) {
+            return response()->json(['error' => 'Unauthorized']);
+        }
+
+        $ip = request()->getClientIp();
+
+        $userLimit = DB::table('view_limits')
+            ->select(['ip', 'usage_count'])
+            ->where('ip', $ip)
+            ->first();
+
+        if (!$userLimit) {
+            // First viewing the article.
+            DB::table('view_limits')
+                ->insert([
+                    'ip'         => $ip,
+                    'created_at' => now(),
+                ]);
+        } elseif ($userLimit->usage_count >= 3) {
+            // Out of free usage.
+            return response()->json(['error' => 'Unauthorized']);
+        } else {
+            // Count usage.
+            DB::table('view_limits')
+                ->select(['ip', 'usage_count'])
+                ->where('ip', $ip)
+                ->increment('usage_count', 1, [
+                    'updated_at' => now()
+                ]);
+        }
+
         $article = Article::where('id', $id)->first();
 
         $ratings = $article->ratings;
